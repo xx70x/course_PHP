4677fed3c181839b2c73c1525227f59e47f0c12a
Ya_Lit <xx70x@ya.ru>
2022-08-05 20:52:48 +0500

    first commit

---
+++ lesson_4.php
@@ -0,0 +1,14 @@
+<?php
+
+$lineBreak = "<br>";
+$greeting = "Welcom";
+$clientName = "Alex";
+echo "{$greeting}, {$clientName}! {$lineBreak}";
+
+$priceOfBookOne = 60;
+$priceOfBookTwo = 17;
+$discount = 30;
+$bookCount = 2;
+
+$totalPrice = $priceOfBookTwo + $priceOfBookOne;
+echo "Total price: {$totalPrice}. {$lineBreak}";
