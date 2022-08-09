c32b5675c5dffa45ea16c9b2e4841ff5fcc90813
Ya_Lit <xx70x@ya.ru>
2022-08-10 01:19:25 +0500

    new

--- practical_task_2.php
+++ practical_task_2.php
@@ -1,3 +1,19 @@
 <?php
 
-$secondsPerHour = 60;
+$lineBrake = "<br>";
+
+$greetings = "Hello";
+$name = "Anna";
+echo "{$greetings}, {$name}!" . $lineBrake;
+
+$poem = "\"Аптеку позабудь ты для венков лавровых<br>
+И не мори больных, но усыпляй здоровых.\"";
+
+echo $poem;
+
+$secondsPerMinute = 60;
+$minutesPerHour = 60;
+$secondsPerHour = $secondsPerMinute * $minutesPerHour;
+
+echo $lineBrake;
+echo "В часе {$secondsPerHour} секунд";     //!не уверен, что так можно
