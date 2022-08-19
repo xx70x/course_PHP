d76def5c511e00b69bcd3204244cf33395899731
Ya_Lit <xx70x@ya.ru>
2022-08-20 00:41:52 +0500

    new

--- lesson_9.php
+++ lesson_9.php
@@ -15,5 +15,28 @@ City: Springville, CA
 INFO;
 
 echo nL2br($information);
+echo "<br>";
 
+//кодировка
+$strEn = "Hello"; //5
+$strRu = "Привет"; //
+
+echo strLen($strEn) . "<br>";
+echo strLen($strRu) . "<br>";
+echo mb_strlen($strRu) . "<br>";
+
+echo $strRu[2] . "<br>";
+echo mb_substr($strRu, 0, 1) . "<br>";
+
+//получение подстроки
+echo strpos("Hello world!", "ello") . "<br>";
+echo mb_strpos("Привет, мир!", "мир") . "<br>";
+
+if (strpos("Hello world!", "Hello")) {
+    echo "Find! <br>";
+}
+
+if (strpos("Hello world!", "Hello") !== false) {
+    echo "Find! <br>";
+}
 
