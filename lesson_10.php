5229ed90da00c60f43d16190e6915dfff92e39c9
Ya_Lit <xx70x@ya.ru>
2022-08-20 01:27:17 +0500

    new

--- lesson_10.php
+++ lesson_10.php
@@ -1,2 +1,18 @@
 <?php
 echo time();
+echo "<br>";
+
+const SECOND_IN_YEAR = 60 * 60 * 24 * 365;
+$currentYear = intval(floor(time()/SECOND_IN_YEAR) + 1970) . "<br>";
+echo $currentYear;
+
+echo date("D");
+
+echo mktime(0, 0, 0, 20, 3, 2023) . "<br>";
+
+echo date_default_timezone_get() . "<br>";
+//echo date_default_timezone_set(Asia/Yekaterinburg) . "<br>";
+echo date_default_timezone_get() . "<br>";
+
+echo date_default_timezone_set("Asia/Yekaterinburg") . "<br>";
+echo date_default_timezone_get() . "<br>";
