7673cb1e2ab848b20f7e759f6a253be1c5e13b22
Ya_Lit <xx70x@ya.ru>
2022-08-19 23:56:16 +0500

    new

--- lesson_9.php
+++ lesson_9.php
@@ -7,3 +7,11 @@ City: Springville, CA
 INFO;
 
 echo ($information . "<br>");
+
+$information = <<<INFO
+Name: John Smith
+Address: 123 Main St
+City: Springville, CA
+INFO;
+
+echo n12br($information);
