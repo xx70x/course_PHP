436354df49f237b7671e0e3985a89ca0acdd901e
Ya_Lit <xx70x@ya.ru>
2022-08-19 21:40:46 +0500

    new

--- lesson_5.php
+++ lesson_5.php
@@ -1,2 +1,23 @@
 <?php
 
+$lineBreak ="<br>";
+print_r("Welcome, friend! {$lineBreak}");
+$straightText = "Howe are you?";
+$reverseText = strrev($straightText);
+print_r("Straight text: {$straightText}. {$lineBreak} Reverse text: {$reverseText}. {$lineBreak}");
+
+$resultOper = 2**3;
+$resultPow = pow(2, 3);
+print_r("Res1: {$resultOper}. {$lineBreak} Res2: {$resultPow}. {$lineBreak}");
+
+
+
+
+
+
+
+
+
+
+
+
