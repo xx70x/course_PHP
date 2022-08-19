07f0609bf2c1bbecb96175d4ec0faed6b04755c0
Ya_Lit <xx70x@ya.ru>
2022-08-19 21:54:06 +0500

    new

--- lesson_5.php
+++ lesson_5.php
@@ -3,20 +3,22 @@
 $lineBreak ="<br>";
 print_r("Welcome, friend! {$lineBreak}");
 $straightText = "Howe are you?";
+
+//стандартные функции
 $reverseText = strrev($straightText);
 print_r("Straight text: {$straightText}. {$lineBreak} Reverse text: {$reverseText}. {$lineBreak}");
-
 $resultOper = 2**3;
 $resultPow = pow(2, 3);
 print_r("Res1: {$resultOper}. {$lineBreak} Res2: {$resultPow}. {$lineBreak}");
 
+//функция без аргументов
+$randomNumber = rand();
+print_r("Random number: {$randomNumber}. {$lineBreak}");
 
-
-
-
-
-
-
+// функции с параметром по умолчанию
+$floatNumber = 56.4356;
+$result = round($floatNumber);
+print_r("Round: {$result}. {$lineBreak}");
 
 
 
